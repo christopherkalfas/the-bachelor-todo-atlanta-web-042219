cd589483c06b9ad8522bfa5cf5edccb5256d01f4
Christopher Kalfas <chriskalfas@yahoo.com>
2019-03-24 19:05:20 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,64 @@
 def get_first_name_of_season_winner(data, season)
-  # code here
+  data[season].each do |constant|
+    constant.each do |key, value| 
+      if value == "Winner"
+        full_name = constant["name"]
+        return full_name.split(' ').first 
+      end
+    end
+  end 
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season, array|
+    array.each do |people|
+      people.each do |k,v|
+        if v == occupation
+          return people["name"]
+        end
+      end
+    end
+  end
+  get_contestant_name(data, occupation)
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0 
+    data.each do |season, contestant|
+      contestant.each do |info|
+        info.each do |key, value|
+          if value == hometown
+            counter += 1 
+          end 
+        end
+      end
+    end
+counter
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, contestant|
+    contestant.each do |info|
+      info.each do |key, value|
+        if value == hometown
+          return info["occupation"]
+        end 
+      end
+    end
+  end 
 end
 
 def get_average_age_for_season(data, season)
-  # code here
-end
+  age = 0 
+  count = 0 
+  data[season].each do |contestant|
+    contestant.each do |attibute, string|
+      if attibute == "age"
+        count += 1 
+        age += string.to_f
+      end
+    end
+  end
+  average_age = (age / count).round 
+return average_age
+end 
