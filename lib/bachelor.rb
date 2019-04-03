21db66cb7a2872d41712215f0303df83ba4c6dc7
Christopher Kalfas <chriskalfas@yahoo.com>
2019-04-03 23:15:04 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -55,7 +55,7 @@ def get_average_age_for_season(data, season)
   data[season].each do  |contestants|
     contestants.each do |attibute, personal_info|
       if attibute == "age"
-        age = personal_info.to_f
+        age += personal_info.to_f
         count +=1 
       end 
     end 
